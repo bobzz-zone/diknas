8b9101681cdbe4d1565c8a075efb16e54a023164
bobzz-zone <bobzz.zone@gmail.com>
2018-06-19 01:36:20 +0700

    code

--- diknas/diknas/doctype/perubahan_stok/perubahan_stok.py
+++ diknas/diknas/doctype/perubahan_stok/perubahan_stok.py
@@ -7,4 +7,38 @@ import frappe
 from frappe.model.document import Document
 
 class PerubahanStok(Document):
-	pass
+	def validate(self):
+		if cint(self.qty)<0:
+			#check kartu stock
+			ks = frappe.db.sql("""select name, sekolah,jenis_barang,qty from `tabKartu Stok` where sekolah="{}" and jenis_barang="{}" """.format(self.sekolah,self.jenis_barang),as_list=1)
+			ada=0
+			for n in ks:
+				ada=1
+				if cint(n[2])<cint(self.qty)*-1:
+					frappe.throw("Stock di sekolah {} untuk {} hanya ada {}".format(self.sekolah, self.jenis_barang,cint(n[2])+cint(self.qty)))
+			if ada==0:
+				doc = frappe.get_doc({
+					"doctype": "Kartu Stok",
+					"jenis_barang":self.jenis_barang,
+					"qty":0,
+					"sekolah":self.sekolah
+					})
+				doc.insert()
+		else :
+			ks = frappe.db.sql("""select name, sekolah,jenis_barang,qty from `tabKartu Stok` where sekolah="{}" and jenis_barang="{}" """.format(self.sekolah,self.jenis_barang),as_list=1)
+			ada=0
+			for n in ks:
+				ada=1
+			if ada==0:
+				doc = frappe.get_doc({
+					"doctype": "Kartu Stok",
+					"jenis_barang":self.jenis_barang,
+					"qty":0,
+					"sekolah":self.sekolah
+					})
+				doc.insert()
+	def on_submit(self):
+		#check kartu stock
+		ks = frappe.db.sql("""update `tabKartu Stok` set qty=qty+({}) where sekolah="{}" and jenis_barang="{}" """.format(self.qty,self.sekolah,self.jenis_barang),as_list=1)				
+	def on_cancel(self):
+		ks = frappe.db.sql("""update `tabKartu Stok` set qty=qty-({}) where sekolah="{}" and jenis_barang="{}" """.format(self.qty,self.sekolah,self.jenis_barang),as_list=1)				
